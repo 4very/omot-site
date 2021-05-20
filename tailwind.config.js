1b8a4ed5d4277c1618aedafd494485fb28a1efb6
avery <asommer999@gmail.com>
2021-05-20 01:18:25 -0400

    added typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  mode: 'jit',
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Open Sans', 'sans-serif'],
-        'mono': ['Source Code Pro', 'mono']
+        sans: ["Open Sans", "sans-serif"],
+        mono: ["Source Code Pro", "mono"],
       },
-      colors:{
-        background: '#161212',
-        accent1: '#9074BE',
-        accent2: '#7EC8AC',
-        accent3: '#FC9F5B',
-        white: '#F7F2F7'
+      colors: {
+        background: "#161212",
+        accent1: "#9074BE",
+        accent2: "#7EC8AC",
+        accent3: "#FC9F5B",
+        white: "#F7F2F7",
       },
       zIndex: {
-        '-1':'-1'
-      }
+        "-1": "-1",
+      },
     },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
