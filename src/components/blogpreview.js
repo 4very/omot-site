16448ec39459a5865ec96917230b04e0f3b0037d
avery <asommer999@gmail.com>
2021-05-20 10:32:00 -0400

    added hashtags from contentful

--- src/components/blogpreview.js
+++ src/components/blogpreview.js
@@ -13,6 +13,7 @@ const Blogpreview = () => {
             title
             publishDate
             slug
+            hashtags
           }
         }
       }
@@ -20,33 +21,32 @@ const Blogpreview = () => {
   `);
 
   return (
-    <div class="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+    <div className="p-5 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5 w-2/3 justify-center">
       {data.allContentfulPhotopost.edges.map((edge) => {
         return (
-          <div class="rounded overflow-hidden shadow-lg">
+          <div className="rounded overflow-hidden shadow-lg bg-gray-200">
             <img
-              class="w-full"
+              className="w-full"
               src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg"
               alt="Mountain"
             />
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">{edge.node.title}</div>
-              <p class="text-gray-700 text-base">
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{edge.node.title}</div>
+              <p className="text-gray-700 text-base">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                 Voluptatibus quia, nulla! Maiores et perferendis eaque,
                 exercitationem praesentium nihil.
               </p>
             </div>
-            <div class="px-6 pt-4 pb-2">
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #photography
-              </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #travel
-              </span>
-              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                #winter
-              </span>
+            <div className="px-6 pt-4 pb-2">
+              {edge.node.hashtags &&
+                edge.node.hashtags.map((hashtag) => {
+                  return (
+                    <span className="inline-block bg-gray-300 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                      #{hashtag}
+                    </span>
+                  );
+                })}
             </div>
           </div>
         );
